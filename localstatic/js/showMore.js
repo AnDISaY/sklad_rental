f95b16f17b6906d7f6d085771674510e91eeda1f
AnDISaY <sainirgg@inbox.ru>
2025-03-13 11:28:03 +0600

    Calendar

--- localstatic/js/showMore.js
+++ localstatic/js/showMore.js
@@ -1,149 +1,152 @@
-arr = ['favorites', 'category']
-pathName = window.location.pathname.slice(1, -1)
-showMoreBtns = document.querySelectorAll('.show-more')
-products = window.products
 
-if (pathName == 'favorites') {
-    arrItem = arr[0]
-} else {
-    arrItem = arr[1]
-}
+if (screen.width > 720) {
+    arr = ['favorites', 'category']
+    pathName = window.location.pathname.slice(1, -1)
+    showMoreBtns = document.querySelectorAll('.show-more')
+    products = window.products
 
-if (products.length > 5) {
-    for (i=0; i < products.slice(5).length; i++) {
-        document.querySelectorAll('.fsd12mjh63').forEach(id => {
-            if (Number(id.innerHTML) == products[i+5].id) {
-                id.closest(`.${arrItem}__card`).remove()
-            }
-        })
+    if (pathName == 'favorites') {
+        arrItem = arr[0]
+    } else {
+        arrItem = arr[1]
     }
-}
 
-showMoreBtns.forEach(btn => btn.addEventListener("click", (e)=> {
-    e.preventDefault()
-    cardsBlock = document.querySelector(`.${arrItem}__cards`)
+    if (products.length > 5) {
+        for (i=0; i < products.slice(5).length; i++) {
+            document.querySelectorAll('.fsd12mjh63').forEach(id => {
+                if (Number(id.innerHTML) == products[i+5].id) {
+                    id.closest(`.${arrItem}__card`).remove()
+                }
+            })
+        }
+    }
 
-    for (let i=0; i < products.slice(5).length; i++) {
-        card = document.createElement('a')
-        card.classList.add(`${arrItem}__card`)
-        card.classList.add(`favorite-card`)
-        card.href = `/product/${products[i+5]['id']}`
+    showMoreBtns.forEach(btn => btn.addEventListener("click", (e)=> {
+        e.preventDefault()
+        cardsBlock = document.querySelector(`.${arrItem}__cards`)
 
-        id = document.createElement('div')
-        id.classList.add('fsd12mjh63')
-        id.style.display = 'none'
-        id.innerHTML = products[i+5].id
-        card.appendChild(id)
+        for (let i=0; i < products.slice(5).length; i++) {
+            card = document.createElement('a')
+            card.classList.add(`${arrItem}__card`)
+            card.classList.add(`favorite-card`)
+            card.href = `/product/${products[i+5]['id']}`
 
-        imageBlock = document.createElement('div')
-        imageBlock.classList.add(`${arrItem}__card__image-block`)
-        favoriteBtn = document.createElement(`div`)
-        favoriteBtn.classList.add(`${arrItem}__card__favorite`)
-        favoriteBtn.classList.add(`favorite-btn`)
+            id = document.createElement('div')
+            id.classList.add('fsd12mjh63')
+            id.style.display = 'none'
+            id.innerHTML = products[i+5].id
+            card.appendChild(id)
 
-        if (window.user_is_authenticated == "True") {
-            svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-            svgEl.setAttribute("width", "100%")
-            svgEl.setAttribute("viewBox", "0 0 22 19")
-            svgEl.setAttribute("fill", "none")
-            svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg")
-            svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-            if (window.favArray.includes(products[i+5]['id'])) {
-                svgEl.classList.add('unfavorite-svg')
-                svgPath.setAttribute("d", "M11 19C10.8887 19 10.7775 18.9713 10.6778 18.9138C10.5695 18.8514 7.99691 17.3599 5.38743 15.1125C3.84082 13.7805 2.60624 12.4594 1.71808 11.1859C0.568758 9.53795 -0.00921315 7.95286 0.000111035 6.47457C0.0110251 4.75441 0.628011 3.13671 1.73755 1.91942C2.86582 0.681629 4.37152 0 5.97739 0C8.03545 0 9.91709 1.15121 11 2.97486C12.083 1.15125 13.9646 0 16.0227 0C17.5398 0 18.9873 0.615036 20.0986 1.73183C21.3183 2.9574 22.0112 4.68906 21.9999 6.48272C21.9905 7.95844 21.4017 9.54113 20.2498 11.1868C19.3589 12.4596 18.1261 13.7801 16.5855 15.1117C13.9856 17.3589 11.4315 18.8504 11.324 18.9128C11.2256 18.9699 11.1138 19 11 19Z")
-                svgPath.setAttribute("fill", "#FF6400")
+            imageBlock = document.createElement('div')
+            imageBlock.classList.add(`${arrItem}__card__image-block`)
+            favoriteBtn = document.createElement(`div`)
+            favoriteBtn.classList.add(`${arrItem}__card__favorite`)
+            favoriteBtn.classList.add(`favorite-btn`)
+
+            if (window.user_is_authenticated == "True") {
+                svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+                svgEl.setAttribute("width", "100%")
+                svgEl.setAttribute("viewBox", "0 0 22 19")
+                svgEl.setAttribute("fill", "none")
+                svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+                svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+                if (window.favArray.includes(products[i+5]['id'])) {
+                    svgEl.classList.add('unfavorite-svg')
+                    svgPath.setAttribute("d", "M11 19C10.8887 19 10.7775 18.9713 10.6778 18.9138C10.5695 18.8514 7.99691 17.3599 5.38743 15.1125C3.84082 13.7805 2.60624 12.4594 1.71808 11.1859C0.568758 9.53795 -0.00921315 7.95286 0.000111035 6.47457C0.0110251 4.75441 0.628011 3.13671 1.73755 1.91942C2.86582 0.681629 4.37152 0 5.97739 0C8.03545 0 9.91709 1.15121 11 2.97486C12.083 1.15125 13.9646 0 16.0227 0C17.5398 0 18.9873 0.615036 20.0986 1.73183C21.3183 2.9574 22.0112 4.68906 21.9999 6.48272C21.9905 7.95844 21.4017 9.54113 20.2498 11.1868C19.3589 12.4596 18.1261 13.7801 16.5855 15.1117C13.9856 17.3589 11.4315 18.8504 11.324 18.9128C11.2256 18.9699 11.1138 19 11 19Z")
+                    svgPath.setAttribute("fill", "#FF6400")
+                } else {
+                    svgEl.classList.add('favorite-svg')
+                    svgPath.setAttribute("d", "M16.2708 0C13.9755 0 11.8919 1.60413 11 3.77331C10.1081 1.60327 8.0245 0 5.72917 0C2.57033 0 0 2.43003 0 5.41644C0 11.2749 8.94117 17.6099 10.7332 18.8197L11 19L11.2668 18.8197C13.0598 17.6099 22 11.274 22 5.41644C22 2.43003 19.4297 0 16.2708 0ZM11 17.9332C6.28742 14.6963 0.916667 9.552 0.916667 5.41644C0.916667 2.90754 3.07542 0.86663 5.72917 0.86663C8.24817 0.86663 10.5417 3.34519 10.5417 6.06641H11.4583C11.4583 3.34519 13.7518 0.86663 16.2708 0.86663C18.9246 0.86663 21.0833 2.90754 21.0833 5.41644C21.0833 9.552 15.7126 14.6963 11 17.9332Z")
+                    svgPath.setAttribute("fill", "currentColor")
+                }
             } else {
-                svgEl.classList.add('favorite-svg')
+                favoriteBtn.classList.add('popup-link')
+                favoriteBtn.setAttribute("link", "favorites-popup")
+                svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+                svgEl.classList.add("favorite-svg-inactive")
+                svgEl.setAttribute("width", "100%")
+                svgEl.setAttribute("viewBox", "0 0 22 19")
+                svgEl.setAttribute("fill", "none")
+                svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+                svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
                 svgPath.setAttribute("d", "M16.2708 0C13.9755 0 11.8919 1.60413 11 3.77331C10.1081 1.60327 8.0245 0 5.72917 0C2.57033 0 0 2.43003 0 5.41644C0 11.2749 8.94117 17.6099 10.7332 18.8197L11 19L11.2668 18.8197C13.0598 17.6099 22 11.274 22 5.41644C22 2.43003 19.4297 0 16.2708 0ZM11 17.9332C6.28742 14.6963 0.916667 9.552 0.916667 5.41644C0.916667 2.90754 3.07542 0.86663 5.72917 0.86663C8.24817 0.86663 10.5417 3.34519 10.5417 6.06641H11.4583C11.4583 3.34519 13.7518 0.86663 16.2708 0.86663C18.9246 0.86663 21.0833 2.90754 21.0833 5.41644C21.0833 9.552 15.7126 14.6963 11 17.9332Z")
                 svgPath.setAttribute("fill", "currentColor")
             }
-        } else {
-            favoriteBtn.classList.add('popup-link')
-            favoriteBtn.setAttribute("link", "favorites-popup")
-            svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-            svgEl.classList.add("favorite-svg-inactive")
-            svgEl.setAttribute("width", "100%")
-            svgEl.setAttribute("viewBox", "0 0 22 19")
-            svgEl.setAttribute("fill", "none")
-            svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg")
-            svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-            svgPath.setAttribute("d", "M16.2708 0C13.9755 0 11.8919 1.60413 11 3.77331C10.1081 1.60327 8.0245 0 5.72917 0C2.57033 0 0 2.43003 0 5.41644C0 11.2749 8.94117 17.6099 10.7332 18.8197L11 19L11.2668 18.8197C13.0598 17.6099 22 11.274 22 5.41644C22 2.43003 19.4297 0 16.2708 0ZM11 17.9332C6.28742 14.6963 0.916667 9.552 0.916667 5.41644C0.916667 2.90754 3.07542 0.86663 5.72917 0.86663C8.24817 0.86663 10.5417 3.34519 10.5417 6.06641H11.4583C11.4583 3.34519 13.7518 0.86663 16.2708 0.86663C18.9246 0.86663 21.0833 2.90754 21.0833 5.41644C21.0833 9.552 15.7126 14.6963 11 17.9332Z")
-            svgPath.setAttribute("fill", "currentColor")
-        }
-        svgEl.appendChild(svgPath)
-        favoriteBtn.appendChild(svgEl)
+            svgEl.appendChild(svgPath)
+            favoriteBtn.appendChild(svgEl)
 
-        imageBlock.appendChild(favoriteBtn)
-        imageEl = document.createElement('div')
-        imageEl.classList.add(`${arrItem}__card__image`)
-        imgTag = document.createElement('img')
-        imgTag.setAttribute("src", `/media/${products[i+5].photo}`)
-        imgTag.setAttribute("alt", "Product image")
-        imageEl.appendChild(imgTag)
-        imageBlock.appendChild(imageEl)
-        card.appendChild(imageBlock)
+            imageBlock.appendChild(favoriteBtn)
+            imageEl = document.createElement('div')
+            imageEl.classList.add(`${arrItem}__card__image`)
+            imgTag = document.createElement('img')
+            imgTag.setAttribute("src", `/media/${products[i+5].photo}`)
+            imgTag.setAttribute("alt", "Product image")
+            imageEl.appendChild(imgTag)
+            imageBlock.appendChild(imageEl)
+            card.appendChild(imageBlock)
 
-        categoryEl = document.createElement("div")
-        categoryEl.classList.add(`${arrItem}__card__category`)
-        categoryEl.classList.add(`text-medium-small`)
-        categoryEl.innerHTML = products[i+5].category__name
-        card.appendChild(categoryEl)
+            categoryEl = document.createElement("div")
+            categoryEl.classList.add(`${arrItem}__card__category`)
+            categoryEl.classList.add(`text-medium-small`)
+            categoryEl.innerHTML = products[i+5].category__name
+            card.appendChild(categoryEl)
 
-        nameEl = document.createElement("div")
-        nameEl.classList.add(`${arrItem}__card__name`)
-        nameEl.classList.add(`text-medium-big`)
-        nameEl.innerHTML = products[i+5].name
-        card.appendChild(nameEl)
+            nameEl = document.createElement("div")
+            nameEl.classList.add(`${arrItem}__card__name`)
+            nameEl.classList.add(`text-medium-big`)
+            nameEl.innerHTML = products[i+5].name
+            card.appendChild(nameEl)
 
-        priceEl = document.createElement("div")
-        priceEl.classList.add(`${arrItem}__card__price`)
-        priceEl.classList.add(`text-regular`)
-        priceEl.innerHTML = products[i+5].price
-        card.appendChild(priceEl)
+            priceEl = document.createElement("div")
+            priceEl.classList.add(`${arrItem}__card__price`)
+            priceEl.classList.add(`text-regular`)
+            priceEl.innerHTML = products[i+5].price
+            card.appendChild(priceEl)
 
-        if (window.cartArr) {
-            for (let c=0; c < window.cartArr.length; c++) {
-                if (window.cartArr[c].product__id == products[i+5].id) {
-                    cartEl = document.createElement("div")
-                    cartEl.classList.add(`${arrItem}__card__cart`)
-                    cartWrap = document.createElement("div")
-                    cartWrap.classList.add(`${arrItem}__card__cart__wrapper`)
-                    cartMinus = document.createElement("div")
-                    cartMinus.classList.add(`${arrItem}__card__cart__minus`)
-                    cartMinus.classList.add(`text-medium-big`)
-                    cartMinus.innerHTML = "-"
-                    cartValue = document.createElement("div")
-                    cartValue.classList.add(`${arrItem}__card__cart__value`)
-                    cartValue.classList.add(`text-medium-big`)
-                    cartValue.innerHTML = window.cartArr[c].quantity
-                    cartPlus = document.createElement("div")
-                    cartPlus.classList.add(`${arrItem}__card__cart__plus`)
-                    cartPlus.classList.add(`text-medium-big`)
-                    cartPlus.innerHTML = "+"
-                    cartWrap.appendChild(cartMinus)
-                    cartWrap.appendChild(cartValue)
-                    cartWrap.appendChild(cartPlus)
-                    cartEl.appendChild(cartWrap)
-                    card.appendChild(cartEl)
+            if (window.cartArr) {
+                for (let c=0; c < window.cartArr.length; c++) {
+                    if (window.cartArr[c].product__id == products[i+5].id) {
+                        cartEl = document.createElement("div")
+                        cartEl.classList.add(`${arrItem}__card__cart`)
+                        cartWrap = document.createElement("div")
+                        cartWrap.classList.add(`${arrItem}__card__cart__wrapper`)
+                        cartMinus = document.createElement("div")
+                        cartMinus.classList.add(`${arrItem}__card__cart__minus`)
+                        cartMinus.classList.add(`text-medium-big`)
+                        cartMinus.innerHTML = "-"
+                        cartValue = document.createElement("div")
+                        cartValue.classList.add(`${arrItem}__card__cart__value`)
+                        cartValue.classList.add(`text-medium-big`)
+                        cartValue.innerHTML = window.cartArr[c].quantity
+                        cartPlus = document.createElement("div")
+                        cartPlus.classList.add(`${arrItem}__card__cart__plus`)
+                        cartPlus.classList.add(`text-medium-big`)
+                        cartPlus.innerHTML = "+"
+                        cartWrap.appendChild(cartMinus)
+                        cartWrap.appendChild(cartValue)
+                        cartWrap.appendChild(cartPlus)
+                        cartEl.appendChild(cartWrap)
+                        card.appendChild(cartEl)
+                    }
                 }
             }
-        }
-        if (card.querySelector(`.${arrItem}__card__cart`)) {
-            
-        } else {
-            cartBtn = document.createElement('div')
-            cartBtn.classList.add(`${arrItem}__card__btn`)
-            cartBtn.classList.add(`text-semibold-mid`)
-            cartBtn.innerHTML = "Добавить в корзину"
-            card.appendChild(cartBtn)
+            if (card.querySelector(`.${arrItem}__card__cart`)) {
+                
+            } else {
+                cartBtn = document.createElement('div')
+                cartBtn.classList.add(`${arrItem}__card__btn`)
+                cartBtn.classList.add(`text-semibold-mid`)
+                cartBtn.innerHTML = "Добавить в корзину"
+                card.appendChild(cartBtn)
+            }
+
+            cardsBlock.appendChild(card)
         }
 
-        cardsBlock.appendChild(card)
-    }
-
-    btn.remove()
-    reloadCartScript()
-    reloadPopupScript()
-    reloadFavoritesScript()
-    // btn.closest(`.${arrItem}__container`).appendChild(btn)
-}))
+        btn.remove()
+        reloadCartScript()
+        reloadPopupScript()
+        reloadFavoritesScript()
+        // btn.closest(`.${arrItem}__container`).appendChild(btn)
+    }))
+}
